26b8ebf6c18f558cd24d3392176457fddc7ee6f3
CodeJack <pawan.mamoria92@gmail.com>
2022-12-30 19:43:38 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// 1. Create two variables, firstCard and secondCard. 
+// Set their values to a random number between 2-11
+
+// 2. Create a variable, sum, and set it to the sum of the two cards
+let firstCard = 2
+let secondCard = 10
+
+let sum = firstCard + secondCard
+console.log(sum)
+
+if (sum < 21) {
+
+    console.log("Do you want to draw card ")
+}
+else if (sum == 21) {
+
+    console.log("🥳")
+}
+else {
+
+    console.log("You Lose")
+}
